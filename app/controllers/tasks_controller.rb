bf65a898fe4b70fd6baf0d28956401a04915f695
Kelly Becker <student@epicodus.com>
2014-03-31 17:13:21 -0700

    adds CRUD to tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,38 @@
 class TasksController < ApplicationController
   def index
     @tasks = Task.all
+    @tasks_not_done = Task.where(:done => false)
+    @tasks_done = Task.where(:done => true)
     render('tasks/index.html.erb')
   end
+
+  def new
+    @task = Task.new
+    render('tasks/new.html.erb')
+  end
+
+  def create
+    @task = Task.new(:name => params[:name], :done => params[:done])
+    if @task.save
+      render('tasks/success.html.erb')
+    else
+      render('tasks/new.html.erb')
+    end
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(:done => params[:done])
+      render('tasks/success.html.erb')
+    else
+      render('tasks/index.html.erb')
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    render('tasks/destroy.html.erb')
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 ToDo::Application.routes.draw do
-  match ('tasks', {:via => :get, :to => 'tasks#index'})
+  match('tasks', {:via => :get, :to => 'tasks#index'})
+  match('tasks/', {:via => :post, :to => 'tasks#create'})
+  match('tasks/new', {:via => :get, :to => 'tasks#new'})
+  match('tasks/:id', {:via => [:patch, :put], :to => 'tasks#update'})
+  match('tasks/:id', {:via => :delete, :to => 'tasks#destroy'})
 end
